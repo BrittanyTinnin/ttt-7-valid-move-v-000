4cbcb1c5b23e81c6a42d7f3cc14e765eaeb9aa93
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-02 12:36:17 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
-# def valid_move?(board, index)
-#   if position_taken?(board, index)
-#     # index.between(0, 8)
-#
-# end# code your #valid_move? method here
+def valid_move?(board, index)
+  (position_taken?(board, index)) && index.between(0, 8) ? true : false
+end# code your #valid_move? method here
 
 def position_taken?(board, index)
   (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
