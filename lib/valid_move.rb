8371f64ff4b6433cf592b7f5e376894a0589149b
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-02 12:00:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,11 @@ def valid_move?(board, index)
   end
 end# code your #valid_move? method here
 
-
 def position_taken?(board, index)
   (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
 end# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
