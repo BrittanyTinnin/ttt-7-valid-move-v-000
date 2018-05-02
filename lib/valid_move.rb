f26aa7f5620d1b0f2295966c74f19054cb4f1ba3
Brittany Tinnin <bntinnin7@gmail.com>
2018-05-02 12:18:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,11 +9,3 @@ end# code your #valid_move? method here
 def position_taken?(board, index)
   (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
 end# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-
-def input_to_index(input)
-  input.to_i - 1
-end
-
-def move(board, index, value="X")
-  board[index] = value
-end
